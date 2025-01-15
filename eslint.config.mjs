68959a0d4544276b77cbe9ddd8d77892979cc350
Aj23Mah <ajaymaharjan758@gmail.com>
2025-01-15 15:56:25 +0545

    edit eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,71 +1,17 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-// import eslintJsonConfig from './eslint.config.json' assert { type: 'json' };
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintJsonConfig from './eslint.config.json' assert { type: "json" };
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const _dirname = dirname(_filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin', 'functional'],
-  extends: [
-    ...compat.extends('next/core-web-vitals', 'next/typescript'),
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:functional/no-mutations',
-    // ...eslintJsonConfig.extends,
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [0, { extensions: ['.js', '.jsx'] }],
-    '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/await-thenable': 'off',
-    'no-var': 'error',
-    'no-param-reassign': 'error',
-    'prefer-const': 'error',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-misused-promises': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    'object-curly-spacing': ['warn', 'always'],
-    '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/no-unsafe-call': 'off',
-    '@typescript-eslint/no-unsafe-return': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/unbound-method': 'off',
-    '@typescript-eslint/no-unsafe-member-access': 'off',
-    '@typescript-eslint/no-unsafe-argument': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    'functional/immutable-data': 'off',
-    '@typescript-eslint/restrict-plus-operands': 'off',
-    '@typescript-eslint/restrict-template-expressions': 'off',
-    'functional/no-let': 'off',
-    'functional/prefer-readonly-type': 'off',
-    quotes: [
-      'warn',
-      'single',
-      {
-        allowTemplateLiterals: true,
-      },
-    ],
-  },
-};
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...eslintJsonConfig,
+];
+
+export default eslintConfig;
